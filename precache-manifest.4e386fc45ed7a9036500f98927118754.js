3535528e08fff09237b3f27854be221a86cb7fd0
Prijun Koirala <prijun@kth.se>
2024-02-19 13:18:21 +0100

    Updates

---
+++ precache-manifest.4e386fc45ed7a9036500f98927118754.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828b99eadf474c0b6b1452bc95203e0b",
+    "url": "/ivis-project-group-5/index.html"
+  },
+  {
+    "revision": "03b5b0f1030ff0f9abf7",
+    "url": "/ivis-project-group-5/static/css/main.983ca8a8.chunk.css"
+  },
+  {
+    "revision": "71c1cef4a20b13ad3a69",
+    "url": "/ivis-project-group-5/static/js/2.7c418840.chunk.js"
+  },
+  {
+    "revision": "fbd6eff631f37653f9670d1ea54910a1",
+    "url": "/ivis-project-group-5/static/js/2.7c418840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a40827d9cd1a1e20b42",
+    "url": "/ivis-project-group-5/static/js/3.35016d19.chunk.js"
+  },
+  {
+    "revision": "03b5b0f1030ff0f9abf7",
+    "url": "/ivis-project-group-5/static/js/main.13e74733.chunk.js"
+  },
+  {
+    "revision": "cdaed0ddf34d71618e00",
+    "url": "/ivis-project-group-5/static/js/runtime-main.fca8e93a.js"
+  }
+]);
